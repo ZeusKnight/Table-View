c3d393bff52aee42853fc2e1075a08024ffb68c6
Sibo <oyu-gio@hotmail.com>
2015-07-22 21:13:03 -0500

    Initial Commit
    Initial Commit

--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet var sliderValue: UISlider!
+    @IBOutlet var myTable: UITableView!
+    
+    @IBAction func mySlider(sender: AnyObject) {
+        myTable.reloadData();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell");
+        
+        let identifier = Int(sliderValue.value * 20);
+        cell.textLabel?.text = String(identifier * indexPath.row + 1);
+        return cell;
+    }
 }
 
